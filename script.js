489a08bf683dfc10302fc41d2bef4d348f813549
Rahmawati Desi Tri Wulandari <rahmawati.desi2002@gmail.com>
2022-06-07 12:36:53 +0700

    tugasku

---
+++ script.js
@@ -0,0 +1,10 @@
+var  tombolMenu = dokumen.getElementsByClassName('tombol-menu')[0];
+var  menu = dokumen.getElementsByClassName('menu')[0];
+
+tombolMenu.klik = fungsi() {
+    menu.daftarkelas.toggle('aktif');
+}
+
+menu.klik = fungsi() {
+    menu.daftarkelas.toggle('aktif');
+}
